4a76b2440217fb7d1644ef8222a56bc493e7a84f
Chieke Emeka <mekus1085@gmail.com>
2023-09-23 13:23:38 +0100

    refactored

--- script.js
+++ script.js
@@ -1,139 +1,131 @@
 'use strict';
 
-// here i am declaring some variables
-const player1Value = document.querySelector('#score--0');
-const player2Value = document.querySelector('#score--1');
-const image = document.querySelector('.dice');
+// changing the total score to 0
+const player0score = document.getElementById('score--0');
+const player1score = document.getElementById('score--1');
+//
+//
+//
+//
+// we create variable for dice
+const dice = document.querySelector('.dice');
 
-// here we declare variable to hold all the buttons
+//
+//
+//
+// roll dice button
 const roll = document.querySelector('.btn--roll');
+// hold button
 const hold = document.querySelector('.btn--hold');
+// new game button
 const newGame = document.querySelector('.btn--new');
+//
+//
+//
+//create a variable for current score
+const currentScore0 = document.getElementById('current--0');
+const currentScore1 = document.getElementById('current--1');
+//
 
-// here we are creating
-let active = 0;
-
-// we declare current score
-const currentScore = document.getElementById('current--' + active);
-
-// here we set players value to zero
-player1Value.textContent = 0;
-player2Value.textContent = 0;
-
-// here we set the value of current score to 0
-currentScore.textContent = 0;
-
-// here we created another value that we can use to perform calculations and the assign it to surrent score
-let current = 0;
-
-// here we created an element for the total score called total
-let total = [0, 0];
-
-// here we removed the dice image by adddng hidden
-image.classList.add('hidden');
-
-// we wan to impliment dice roll
-roll.addEventListener('click', function () {
-  // random number from 1-6 is generated
-  let num = Math.trunc(Math.random() * 6) + 1;
-
-  // we assign each number to the dice
-  image.src = `dice-${num}.png`;
-
-  // we make dice visible by
-  image.classList.remove('hidden');
-
-  // we use if statement to know whether the number is 1 or not
-  if (num !== 1) {
-    // here we add the dice rolled to the current value
-    current = current + num;
-    document.getElementById(`current--${active}`).textContent = current;
-  } else {
-    // here we put conditions when the dice is == 0
-    current = 0;
-    document.getElementById(`current--${active}`).textContent = current;
-    active = active === 0 ? 1 : 0;
-    document.getElementById(`current--${active}`).textContent;
-
-    // here we changed the background color of the active player
-    document.querySelector('.player--0').classList.toggle('player--active');
-    document.querySelector('.player--1').classList.toggle('player--active');
-
-    document
-      .querySelector(`.player--${active}`)
-      .classList.add('player--active');
-  }
-});
-
-// here we implement hold
-hold.addEventListener('click', function () {
-  // here we assign the value of current score to players score
-
-  total[active] += current;
-  document.querySelector(`#score--${active}`).textContent = total[active];
-
-  // here we check if the score is <= 100
-  if (document.querySelector(`#score--${active}`).textContent <= 100) {
-    // here we set the value of current to 0 and assgn t to current score this will happen when hold is pressed
-    current = 0;
-    document.getElementById(`current--${active}`).textContent = current;
-
-    // here we used ternary operator to to switch players
-    active = active === 0 ? 1 : 0;
-
-    // here we changed the background color of the active player, wefirst checked if anyof the element have the class player--active  and remove it
-    document.querySelector('.player--0').classList.toggle('player--active');
-    document.querySelector('.player--1').classList.toggle('player--active');
-
-    // here we added it to the active player
-    document
-      .querySelector(`.player--${active}`)
-      .classList.add('player--active');
-  } else {
-    // if the total score is over 100 the active player win, so we have to hide the dice
-    image.classList.add('hidden');
-
-    //here we add the class plyerer--winner so the background can change
-    document
-      .querySelector(`.player--${active}`)
-      .classList.add('player--winner');
-    document.querySelector(`.player--${active}`).classList.add('name');
-
-    // here we turned off the roll button and also make the dice hide, and also de activate the active players background
-    roll.addEventListener('click', function () {
-      current = 0;
-      document.getElementById(`current--${active}`).textContent = current;
-      image.classList.add('hidden');
-      document.querySelector('.player--0').classList.remove('player--active');
-      document.querySelector('.player--1').classList.remove('player--active');
-    });
-
-    //here we turned off the hold button and also make the dice hide, and also de activate the active players background
-    hold.addEventListener('click', function () {
-      total[active] += current;
-      document.querySelector(`#score--${active}`).textContent = total[active];
-      image.classList.add('hidden');
-      document.querySelector('.player--0').classList.remove('player--active');
-      document.querySelector('.player--1').classList.remove('player--active');
-    });
-
-    document.querySelector('.player--0').classList.remove('player--active');
-    document.querySelector('.player--1').classList.remove('player--active');
-  }
-});
-
-newGame.addEventListener('click', function () {
-  // here we set the current score to 0
+//
+//
+let totalScore, activePlayer, current, active;
+//
+//
+// initialization function is created
+const init = function () {
+  // total score array
+  totalScore = [0, 0];
+  //active player
+  activePlayer = 0;
+  // current score
   current = 0;
-  document.getElementById('current--0').textContent = current;
-  document.getElementById('current--1').textContent = current;
+  // conditions for the button to press
+  active = true;
+  // hiding the dice img
+  dice.classList.add('hidden');
+  // setting players score which is the total score to 0
+  player0score.textContent = 0;
+  player1score.textContent = 0;
+  document.querySelector(`.player--0`).classList.remove('player--winner');
+  document.querySelector(`.player--1`).classList.remove('player--winner');
+  document.querySelector(`.player--0`).classList.add('player--active');
+  document.querySelector(`.player--1`).classList.remove('player--active');
+  document.getElementById(`current--0`).textContent = current;
+  document.getElementById(`current--1`).textContent = current;
+};
+//
+//
+init();
+// roll dice button put in a function
 
-  total[0] = 0;
-  total[1] = 0;
-  document.querySelector('#score--0').textContent = total[0];
-  document.querySelector('#score--1').textContent = total[1];
+roll.addEventListener('click', function () {
+  if (active) {
+    // get the random number
+    let random = Math.trunc(Math.random() * 6) + 1;
 
-  document.querySelector('.player--0').classList.add('player--active');
-  document.querySelector('.player--1').classList.remove('player--active');
-  active = 0;
+    // assign the random number to dice picture
+    dice.src = `dice-${random}.png`;
+
+    // display dice image
+    document.querySelector('.dice').classList.remove('hidden');
+
+    // checking if the dice number is 1
+    if (random !== 1) {
+      // add the dice number to current score
+      current += random;
+      document.getElementById(`current--${activePlayer}`).textContent = current;
+    } else {
+      // current score is returned to 0
+      current = 0;
+      document.getElementById(`current--${activePlayer}`).textContent = current;
+      // we switch players
+      activePlayer = activePlayer === 0 ? 1 : 0;
+      document.getElementById(`current--${activePlayer}`).textContent;
+
+      // switch player color is implimented
+      document.querySelector(`.player--0`).classList.toggle('player--active');
+      document.querySelector(`.player--1`).classList.toggle('player--active');
+    }
+  }
 });
+// hold dice button
+hold.addEventListener('click', function () {
+  totalScore[activePlayer] += current;
+  // condition to check if the total score is up to 100
+  if (totalScore[activePlayer] >= 100) {
+    // the active player won
+    document
+      .querySelector(`.player--${activePlayer}`)
+      .classList.add('player--winner');
+
+    // dice is hidden again when any player win
+    document.querySelector('.dice').classList.add('hidden');
+    document.getElementById(`score--${activePlayer}`).textContent =
+      totalScore[activePlayer];
+    current = 0;
+    document.getElementById(`current--${activePlayer}`).textContent = current;
+    //
+
+    // we create a condition that will amke all the button unclickable
+    active = false;
+  } else {
+    // if the totalscore is not up to 100 the game continues
+    // current score is added to the total score
+    document.getElementById(`score--${activePlayer}`).textContent =
+      totalScore[activePlayer];
+
+    // current score is returned to 0
+    current = 0;
+    document.getElementById(`current--${activePlayer}`).textContent = current;
+
+    // player is switched
+    activePlayer = activePlayer === 0 ? 1 : 0;
+    document.getElementById(`current--${activePlayer}`).textContent;
+    document.querySelector(`.player--0`).classList.toggle('player--active');
+    document.querySelector(`.player--1`).classList.toggle('player--active');
+  }
+});
+
+// new game button
+newGame.addEventListener('click', init);
